ce350be73f78eb726f5e118cc481dd16774a2fc0
Arthur Botelho <emilioarthur19@gmail.com>
2023-06-20 21:18:35 -0300

    bandeiras API

--- pais.js
+++ pais.js
@@ -1,1 +1,13 @@
-let pais
+let pais = document.getElementById("pais")
+
+let bandeira
+
+fetch("https://restcountries.com/v3.1/name/" + pais)
+    .then(response => response.json())
+    .then(response => exibir(response))
+
+function exibirBandeira(bandeira) {
+    let bandeira = document.createElement("img")
+    document.body.append(bandeira)
+}
+
